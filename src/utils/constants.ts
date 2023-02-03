d1d75507508e287bacd022119c8b2924ea3aab23
Erick Vargas <erianvc.dev@gmail.com>
2023-02-02 22:29:15 -0500

    feat: adds social networks in the initial section

---
+++ src/utils/constants.ts
@@ -0,0 +1,14 @@
+export const SOCIALS_NETWORKS = [
+  {
+    name: "linkedin",
+    url: "https://www.linkedin.com/in/eriandev/",
+  },
+  {
+    name: "github",
+    url: "https://github.com/eriandev",
+  },
+  {
+    name: "twitter",
+    url: "https://twitter.com/eriandev",
+  },
+];
