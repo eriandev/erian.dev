aadcfae4f754497edeaac2f593257198dbb98c79
Erick Vargas <erianvc.dev@gmail.com>
2025-03-22 22:14:16 -0500

    refactor: change const & astro configs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 import icon from 'astro-icon'
+import { loadEnv } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'astro/config'
 
+const { BASE_PATH: site } = loadEnv(process.env.NODE_ENV, process.cwd(), '')
+
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://erian.dev',
+  site,
   integrations: [icon()],
   vite: {
     plugins: [tailwindcss()]
