b0cb074e3a5ab68de3bd6d6a0f18ef6b380f494e
Erick Vargas <erianvc.dev@gmail.com>
2025-03-27 21:54:06 -0500

    fix: increases the depth of Contentful's response

--- src/services/contentful.ts
+++ src/services/contentful.ts
@@ -3,6 +3,6 @@ import { PG_MAIN_ENTRY_ID } from '@/utils/consts'
 import type { PgMainContent } from '@/services/types'
 
 export async function getPgMainContent(): Promise<PgMainContent> {
-  const { fields } = await client.getEntry(PG_MAIN_ENTRY_ID)
+  const { fields } = await client.getEntry(PG_MAIN_ENTRY_ID, { include: 10 })
   return fields as unknown as PgMainContent
 }
