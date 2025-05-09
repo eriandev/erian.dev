93f287773c8cc9c5a8f540992e65a9be439d705f
Erick Vargas <erianvc.dev@gmail.com>
2025-05-09 15:31:54 -0500

    refactor: improves layout

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,7 +1,7 @@
 /// <reference types="astro/client" />
 
 interface ImportMetaEnv {
-  readonly BASE_PATH: string
+  readonly ORIGIN: string
   readonly CONTENTFUL_SPACE_ID: string
   readonly CONTENTFUL_ACCESS_TOKEN: string
   readonly PG_MAIN_ENTRY_ID: string
--- src/utils/consts.ts
+++ src/utils/consts.ts
@@ -1,4 +1,4 @@
-export const BASE_PATH = import.meta.env.BASE_PATH
+export const ORIGIN = import.meta.env.ORIGIN
 export const CONTENTFUL_SPACE_ID = import.meta.env.CONTENTFUL_SPACE_ID
 export const CONTENTFUL_ACCESS_TOKEN = import.meta.env.CONTENTFUL_ACCESS_TOKEN
 export const PG_MAIN_ENTRY_ID = import.meta.env.PG_MAIN_ENTRY_ID
