507339ad538d7f8e3233472a64becf5eb50fe8bb
Erick Vargas <mail@erian.dev>
2026-09-06 22:58:02 -0500

    feat: add JSON-LD

--- src/utils/consts.ts
+++ src/utils/consts.ts
@@ -5,3 +5,27 @@ export const EXTERNAL_API = import.meta.env.EXTERNAL_API
 export const PG_MAIN_ENTRY_ID = import.meta.env.PG_MAIN_ENTRY_ID
 export const CONTENTFUL_SPACE_ID = import.meta.env.CONTENTFUL_SPACE_ID
 export const CONTENTFUL_ACCESS_TOKEN = import.meta.env.CONTENTFUL_ACCESS_TOKEN
+
+export const structuredData = {
+  '@context': 'https://schema.org',
+  '@graph': [
+    {
+      '@type': 'WebSite',
+      '@id': 'https://erian.dev/#website',
+      url: 'https://erian.dev/',
+      name: 'Erick Vargas',
+      description: 'Frontend Developer portfolio',
+      publisher: {
+        '@id': 'https://erian.dev/#person',
+      },
+    },
+    {
+      '@type': 'Person',
+      '@id': 'https://erian.dev/#person',
+      name: 'Erick Vargas',
+      url: 'https://erian.dev/',
+      jobTitle: 'Frontend Developer',
+      sameAs: ['https://github.com/eriandev', 'https://www.linkedin.com/in/eriandev'],
+    },
+  ],
+} as const
